99e98154fe8b9da64557eebb7f7129ea3c3343eb
Daniel Sobczak <danielsobczak@outlook.com>
2021-05-02 14:12:51 +0200

    fixed tests for registration

--- app/src/test/java/com/example/simpleregistrationapp/feature/registration/RegisterNewUserUseCaseImplTest.kt
+++ app/src/test/java/com/example/simpleregistrationapp/feature/registration/RegisterNewUserUseCaseImplTest.kt
@@ -2,6 +2,7 @@ package com.example.simpleregistrationapp.feature.registration
 
 import com.example.simpleregistrationapp.domain.user.UserStorage
 import com.example.simpleregistrationapp.feature.registration.validation.UserValidator
+import com.example.simpleregistrationapp.feature.registration.validation.ValidationResponse
 import com.example.simpleregistrationapp.feature.utils.dummyUser
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.toList
@@ -10,6 +11,7 @@ import kotlinx.coroutines.test.runBlockingTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.given
 import org.mockito.kotlin.mock
 
 @ExperimentalCoroutinesApi
@@ -29,12 +31,38 @@ class RegisterNewUserUseCaseImplTest {
     }
 
     @Test
-    fun registeringNewUserWithAllValidData() = testDispatcher.runBlockingTest {
-        val emits = sut.registerNewUser(dummyUser).toList()
-        assertThat(emits).containsExactly(
-            RegistrationResult.Loading,
-            RegistrationResult.Success
-        )
-    }
+    fun registeringNewUserWithAllFieldsValid_willEmitSuccess() =
+        testDispatcher.runBlockingTest {
+            val emits = sut.registerNewUser(dummyUser).toList()
+            assertThat(emits).containsExactly(
+                RegistrationResult.Loading,
+                RegistrationResult.Success
+            )
+        }
 
+    @Test
+    fun givenDatabaseError_WhenRegisteringNewUserWithAllFieldsValid_willEmitError() =
+        testDispatcher.runBlockingTest {
+            val databaseException = RuntimeException()
+            given(mockUserStorage.insert(dummyUser)).willThrow(databaseException)
+            val emits = sut.registerNewUser(dummyUser).toList()
+            assertThat(emits).containsExactly(
+                RegistrationResult.Loading,
+                RegistrationResult.UnhandledError(databaseException)
+            )
+        }
+
+    @Test
+    fun whenRegisteringNewUserWithMissingMail_willEmitValidationError() =
+        testDispatcher.runBlockingTest {
+            val emits = sut.registerNewUser(dummyUser.copy(email = "")).toList()
+            assertThat(emits).containsExactly(
+                RegistrationResult.Loading,
+                RegistrationResult.InvalidFields(
+                    ValidationResponse.ValidationFailed(
+                        emailError = ValidationResponse.ValidationError.EmptyField
+                    )
+                )
+            )
+        }
 }
--- app/src/test/java/com/example/simpleregistrationapp/feature/utils/user.kt
+++ app/src/test/java/com/example/simpleregistrationapp/feature/utils/user.kt
@@ -3,4 +3,4 @@ package com.example.simpleregistrationapp.feature.utils
 import com.example.simpleregistrationapp.domain.user.User
 import java.util.*
 
-val dummyUser = User("a", "b", Date())
+val dummyUser = User("Foo", "foo@bar.com", Date())
